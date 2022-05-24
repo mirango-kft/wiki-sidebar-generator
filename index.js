cfd272c617113329df7801f60842209602fb68da
Zsolt Paral <104834019+zsparal-mirango@users.noreply.github.com>
2022-05-24 23:35:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function compareItems(a, b) {
     }
 
     if (a.name < b.name) {
-        return -1;
+        return 1;
     }
     
     if (a.name > b.name) {
-        return 1;
+        return -1;
     }
 
     return 0;
